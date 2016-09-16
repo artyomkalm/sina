f91460d3b4b9dacd72b56b75956e63f4c871e130
artkalm <a.kalmykov@tarifer.ru>
2016-09-16 22:48:16 +0300

    test settings

--- test.rb
+++ test.rb
@@ -1,3 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+require 'minitest/autorun'
+require 'rack/test'
+require_relative 'application.rb'
+
+include Rack::Test::Methods
+
+def app
+  Sinatra::Application
+end
+
 describe "Application" do
 
   it "should return json" do
